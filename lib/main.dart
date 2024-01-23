e45f424714ffc1d0af4990a813da6136aca9b02e
Praise Chiedozie Sunday <66026084+Psunday2000@users.noreply.github.com>
2024-01-23 17:41:09 +0100

    Configure FIrebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chicken Court',
       theme: ThemeData(
         // This is the theme of your application.
         //
